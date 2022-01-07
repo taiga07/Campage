2c04bb12c874a91aa52463424138c875102f95cd
taiga07 <taigatani@me.com>
2022-01-07 16:58:47 +0000

    [add]postshow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters,if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    posts_path
+  end
+
+  def after_sign_up_path_for(resource)
+    posts_path
+  end
+
   protected
 
   def configure_permitted_parameters
