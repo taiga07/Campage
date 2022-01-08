04997f7639ab6dd582231e76b72b4733a5bb15a1
taiga07 <taigatani@me.com>
2022-01-08 16:11:47 +0000

    [add]like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,18 @@
 class LikesController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @like = Like.new(post_id: @post.id)
+    @like.user_id = current_user.id
+    @like.save
+    redirect_to post_path(@post.id)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @like = current_user.likes.find_by(post_id: @post.id)
+    @like.destroy
+    redirect_to post_path(@post.id)
+  end
+
 end
