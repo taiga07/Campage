9096fbd88cb6c52ae7a614bff3890869f44d5d37
taiga07 <taigatani@me.com>
2022-01-08 13:13:20 +0000

    [add]comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    # commentsテーブルのuser_idに自分のIDを格納
+    @comment.user_id = current_user.id
+    # commentsテーブルのpost_idにコメントする投稿のIDを格納
+    @comment.post_id = @post.id
+    @comment.save
+    redirect_to post_path(@post.id)
+  end
+
+  def destroy
+    # commentをidやpost_idから見つけて削除する。
+    Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    # 削除後、その投稿の詳細画面に戻る。
+    redirect_to post_path(params[:post_id])
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ApplicationRecord
-  
+
   belongs_to :user
-  
+
   has_many :post_images,dependent: :destroy
+  has_many :comments, dependent: :destroy
   # postモデルを使ってpost_imagesテーブルにあるimage_idカラムに画像を保存すると言う解釈かな
   accepts_attachments_for :post_images, attachment: :image
-  
+
 end
