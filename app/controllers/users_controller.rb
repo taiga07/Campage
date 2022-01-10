418034fe2b9b17a2a7c948dc653124a5aedccb87
taiga07 <taigatani@me.com>
2022-01-10 13:53:11 +0000

    [update]dm

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,25 +5,16 @@ class UsersController < ApplicationController
     @posts = @user.posts  #そのユーザーに紐づいた投稿を@postsに格納
     @following_users = @user.following_user  #そのユーザーがフォローした相手を格納
     @follower_users = @user.follower_user  #そのユーザーがフォローされている相手を格納
-    #entriesテーブルにログインユーザーとDMボタンを押されたユーザーを、
-    #記憶する必要があるので、whereメソッドで探し格納している。
-    @currentuserentry=Entry.where(user_id: current_user.id)  #自分が参加しているメッセージルーム情報を取得する
-    @userentry=Entry.where(user_id: @user.id)  #選択したユーザのメッセージルーム情報を取得する
-    unless @user.id == current_user.id  #自分自身とはDMができないため
-      @currentuserentry.each do |cu|  #current_userが参加していルームをひとつずつ取り出す
-        @userentry.each do |u|  #@user（選択したユーザー）が参加しているルームをひとつずつ取り出す
-          if cu.room_id == u.room_id  #current_userと@userのルームが同じか判断（既にルームが存在するかどうか）
-            @isroom = true  #falseの時(同じじゃない時)の条件を記述するために変数に代入
-            @roomid = cu.room_id  #ルームが共通しているcurrent_userと@userに対して変数を指定
-          end
-        end
-      end
-      unless @isroom  #unlessで@isroomがfalseの時という意味（roomが存在しない時）
-        @room = Room.new  #新しいインスタンスを生成
-        @entry = Entry.new  #新しいインスタンスを生成
-      end
+    
+    @self_room = Entry.where(user_id: current_user.id).pluck(:room_id)
+    @target_room = Entry.where(user_id: params[:id]).pluck(:room_id)
+    @roomid = @self_room & @target_room
+    if @roomid.blank?
+      @room = Room.new  #新しいインスタンスを生成
+      @entry = Entry.new  #新しいインスタンスを生成
+    else
+      @isroom = true
     end
-
   end
 
   def edit
