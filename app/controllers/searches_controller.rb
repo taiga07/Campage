482cebd90257b1be11ad9d3f6ec1f564c7a84719
taiga07 <taigatani@me.com>
2022-01-17 14:59:43 +0000

    [update]layout

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,7 @@ class SearchesController < ApplicationController
   def search
     @model = params["model"]
     @content = params["content"]
-    @records = search_for(@model, @content)
+    @records = search_for(@model, @content).page(params[:page]).per(8)
   end
 
 
@@ -12,7 +12,7 @@ class SearchesController < ApplicationController
     if model == 'user'
         User.where('name LIKE ?', '%'+content+'%')
     elsif model == 'post'
-        Post.where('title LIKE ?', '%'+content+'%').where('body LIKE ?', '%'+content+'%')
+        Post.where('title LIKE ?', '%'+content+'%').or(Post.where('body LIKE ?', '%'+content+'%'))
     end
   end
 
