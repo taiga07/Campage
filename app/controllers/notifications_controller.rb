5cfebba09258d2b082ee2cf77a3b28db3d11e705
taiga07 <taigatani@me.com>
2022-01-17 17:38:35 +0000

    [layout]notification

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,13 +1,13 @@
 class NotificationsController < ApplicationController
-  
+
   def index
-    
-    @notifications = current_user.passive_notifications  # 通知をしてくれたユーザーを取得
+
+    @notifications = current_user.passive_notifications.page(params[:page]).per(10)  # 通知をしてくれたユーザーを取得
     @notifications.where(checked: false).each do |no|  # 通知を未確認のものだけ取り出し
       no.update_attributes(checked: true)  # 確認済みにする
     end
   end
-  
-  
-  
+
+
+
 end
